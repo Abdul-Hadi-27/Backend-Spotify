409b8975711a86ae1f20769e02b6fe36488b3635
Abdul Hadi <abdulhadi272003@gmail.com>
2026-04-23 15:09:42 +0530

    fix port

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 const app=require('./src/app')
 
 require('dotenv').config()
- app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
 
- })
+ const PORT = process.env.PORT || 3000;
+//  app.listen(3000,()=>{
+//     console.log('Server is running on port 3000')
 
+//  })
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
 
